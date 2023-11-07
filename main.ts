7edd325775589f230d43101f8318b17f425b0e6a
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-11-07 13:11:17 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Ihor Chernyshev
+ * Created on: Nov 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+// press "A" button
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    while (loopCounter <= 5) {
+        basic.pause(500)
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+// press "B" button
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    loopCounter = 5
+    sprite = game.createSprite(5, 5)
+    while (loopCounter >= -1) {
+        basic.pause(500)
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
