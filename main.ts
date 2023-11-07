2f836a1ffdb2fe3c0ebefad9fc683c75f93b011e
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-11-07 13:15:52 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -16,31 +16,31 @@ basic.clearScreen()
 
 // press "A" button
 input.onButtonPressed(Button.A, function () {
-    // setup
-    basic.clearScreen()
-    loopCounter = 0
-    sprite = game.createSprite(0, 0)
-    while (loopCounter <= 5) {
-        basic.pause(500)
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter = loopCounter + 1
-    }
-    sprite.delete()
-    basic.showIcon(IconNames.Happy)
+  // setup
+  basic.clearScreen()
+  loopCounter = 0
+  sprite = game.createSprite(0, 0)
+  while (loopCounter <= 5) {
+    basic.pause(500)
+    sprite.set(LedSpriteProperty.X, loopCounter)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    loopCounter = loopCounter + 1
+  }
+  sprite.delete()
+  basic.showIcon(IconNames.Happy)
 })
 
 // press "B" button
 input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    loopCounter = 5
-    sprite = game.createSprite(5, 5)
-    while (loopCounter >= -1) {
-        basic.pause(500)
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter = loopCounter - 1
-    }
-    sprite.delete()
-    basic.showIcon(IconNames.Happy)
+  basic.clearScreen()
+  loopCounter = 5
+  sprite = game.createSprite(5, 5)
+  while (loopCounter >= -1) {
+    basic.pause(500)
+    sprite.set(LedSpriteProperty.X, loopCounter)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
+    loopCounter = loopCounter - 1
+  }
+  sprite.delete()
+  basic.showIcon(IconNames.Happy)
 })
