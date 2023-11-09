a9410ca6321db4523ebd970cc1d21699624776ff
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-11-09 18:29:20 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,14 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ihor Chernyshev
+Created on: Nov 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+loop_counter = 0
 
-display.scroll("Hello, World!")
+display.show(Image.HAPPY)
+display.clear()
+
+input.
