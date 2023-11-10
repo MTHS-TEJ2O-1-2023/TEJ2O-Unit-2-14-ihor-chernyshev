c4ef2fba874e8df0a8649160bccbc1c97f0f4edd
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-11-10 02:09:25 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -4,25 +4,32 @@ Created on: Nov 2023
 This program shows a while loop
 """
 
+# import libraries
 from microbit import *
 
+# variables
 loop_counter = 0
 
+# setup
 display.show(Image.HAPPY)
 display.clear()
 
 while True:
+    # button A
     if button_a.was_pressed():
         loop_counter = 0
         display.clear()
+        # goes from the upper left corner to lower right corner
         while loop_counter <= 5:
             sleep(500)
             display.set_pixel(loop_counter, loop_counter, 9)
             loop_counter = loop_counter + 1
         display.show(Image.HAPPY)
+    # button B
     if button_b.was_pressed():
         loop_counter = 5
         display.clear()
+        # goes from the lower right corner to the upper left corner
         while loop_counter <= 5:
             sleep(500)
             display.set_pixel(loop_counter, loop_counter, 9)
