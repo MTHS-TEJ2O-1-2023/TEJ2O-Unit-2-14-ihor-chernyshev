3759d9525f3989a9a7b21deeef4992b043e5353e
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-11-10 02:04:15 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,7 +1,7 @@
 """
 Created by: Ihor Chernyshev
 Created on: Nov 2023
-This module is a Micro:bit MicroPython program
+This program shows a while loop
 """
 
 from microbit import *
@@ -11,4 +11,20 @@ loop_counter = 0
 display.show(Image.HAPPY)
 display.clear()
 
-input.
+while True:
+    if button_a.was_pressed():
+        loop_counter = 0
+        display.clear()
+        while loop_counter <= 5:
+            sleep(500)
+            display.set_pixel(loop_counter, loop_counter, 9)
+            loop_counter = loop_counter + 1
+        display.show(Image.HAPPY)
+    if button_b.was_pressed():
+        loop_counter = 5
+        display.clear()
+        while loop_counter <= 5:
+            sleep(500)
+            display.set_pixel(loop_counter, loop_counter, 9)
+            loop_counter = loop_counter - 1
+        display.show(Image.HAPPY)
